e60eca9712022f1bec02a78aff23b0ab70be0638
zak00aria <kdzzaki@gmail.com>
2025-03-10 06:32:11 +0000

    implement enumerables

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,72 @@
 module Enumerable
-  # Your code goes here
+  def my_each_with_index
+    index=0
+    for item in self do
+      yield(item, index)
+      index+=1
+    end
+  end
+  def my_select
+    selected_items = []
+    for item in self do
+      if yield(item) == true
+        selected_items.push(item)
+      end
+    end
+    return selected_items
+  end
+  def my_all?
+    for item in self do
+      if !yield(item)
+        return false
+      end
+    end
+    return true
+  end
+  def my_any?
+    for item in self do
+      if yield(item)
+        return true
+      end
+    end
+    return false
+  end
+  def my_none?
+    for item in self do
+      if yield(item)
+        return false
+      end
+    end
+    return true
+  end
+  def my_count
+    counter = 0
+    return self.size if !block_given?
+    for item in self do
+      if yield(item)
+        (counter += 1)
+      end
+    end
+    return counter
+  end
+  def my_map
+    new_items = []
+    for item in self do
+      new_item = item
+      new_item = yield(new_item)
+      new_items.push(new_item)
+    end
+    return new_items
+  end
+  def my_inject(accumulator)
+    if accumulator == nil
+      accumulator = 0
+    end
+    for item in self do
+      accumulator = yield(accumulator, item)
+    end
+    return accumulator
+  end
 end
 
 # You will first have to define my_each
@@ -7,5 +74,9 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    for item in self do
+      yield(item)
+    end
+  end
 end
